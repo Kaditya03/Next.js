24a54b4e77585eba8f7eaaa28461eaace17fa668
Kumar Aditya <kumaraditya93134@gmail.com>
2025-09-21 19:03:17 +0530

    routing

---
+++ src/app/(marketing)/revenue/page.tsx
@@ -0,0 +1,3 @@
+export default function RevenuePage(){
+    return <h1>welcome to the revenue page!</h1>;
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,26 @@
-export default async function ProductDetails({params,}:
-    {params:Promise<{productId:string}>;}
+import {Metadata } from "next";
 
-){
-    const productId = (await params).productId;
-    return <h1>Details about Product {productId}</h1>;
-}
+type Props ={
+    params:Promise<{ productId:string}>;
+};
+
+export const generateMetadata = async ({
+    params,
+}: Props):Promise<Metadata> =>{
+    const id = (await params).productId;
+  const title = await new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve(`iPhone ${id}`);
+    },100);
+  })
+  return {
+    title:`Product ${title}`,
+  }
+};
+
+
+export default async function ProductDetails({params}:Props){
+    
+      const productId = (await params).productId;
+      return <h1>Details about Product {productId}</h1>;
+  }
